637517b47b4eee3b136def8af64ee96a87476f3c
DaniPerezUJI <90398305+DaniPerezUJI@users.noreply.github.com>
2024-10-29 11:57:54 +0100

    initial movement

---
+++ Scripts/vehicle_movement.gd
@@ -0,0 +1,10 @@
+extends VehicleBody3D
+
+
+@export var MAX_STEER = 0.9
+@export var ENGINE_POWER = 300
+
+
+func _physics_process(delta: float) -> void:
+	steering = move_toward(steering, Input.get_axis("ui_right", "ui_left") * MAX_STEER, delta * 10)
+	engine_force = Input.get_axis("ui_decel", "ui_accel") * ENGINE_POWER
