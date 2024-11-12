739a7e1fc310213205b297241d147a7edd03fc3d
DaniPerezUJI <90398305+DaniPerezUJI@users.noreply.github.com>
2024-11-12 12:27:58 +0100

    car sticks to floor

--- Scripts/vehicle_movement.gd
+++ Scripts/vehicle_movement.gd
@@ -7,47 +7,26 @@ const STEER_LIMIT : float = 0.4
 @export var ENGINE_POWER : float = 300
 #@export var MAX_ENGINE_POWER : float = 500
 const BRAKE_STRENGTH : float = 2.0
+const DOWN_FORCE : float = -2000.0
 
 var previous_speed := linear_velocity.length()
 var _steer_target := 0.0
-
 var initial_min_speed : float = 5.0
+var floor_raycast : RayCast3D 
 
-var left_right_input : int
 func _ready() -> void:
+	floor_raycast = $raycast_floor
+	floor_raycast.enabled = true
 	pass
 	
 func _physics_process(delta: float) -> void:
-	
-	steering = move_toward(steering, Input.get_axis("ui_right", "ui_left") * MAX_STEER, delta * 10)
+	var fwd_mps := (linear_velocity * transform.basis).x
 	#engine_force = Input.get_axis("ui_decel", "ui_accel") * ENGINE_POWER
 	
 	var actual_speed := linear_velocity.length()
 	var direction := global_transform.basis.z.dot(linear_velocity.normalized())
-		
-	# initial accel faster
-	#if Input.is_joy_button_pressed(player_index, ):
-	if Input.is_action_pressed("ui_accel"):
-		
-		#print(int(actual_speed)) #DEBUG
-		if actual_speed < initial_min_speed && not is_zero_approx(actual_speed):
-			#print("Max accel") #DEBUG
-			#print(Input.get_action_strength("ui_accel"))
-			engine_force = Input.get_action_strength("ui_accel") * ENGINE_POWER * 2.5
-		else:
-			engine_force = Input.get_action_strength("ui_accel") * ENGINE_POWER
-	else:
-		engine_force = 0.0
-		
-	if Input.is_action_pressed("ui_decel"):
-
-		if direction > 0.0:
-			engine_force = -Input.get_action_strength("ui_decel") * ENGINE_POWER * BRAKE_STRENGTH
-		elif actual_speed < initial_min_speed && not is_zero_approx(actual_speed):
-			engine_force = -Input.get_action_strength("ui_decel") * ENGINE_POWER * 2.0
-		else:
-			engine_force = -Input.get_action_strength("ui_decel") * ENGINE_POWER
-	# vibrate with collision
+	
+	# drastic speed change -> collision
 	if abs(actual_speed - previous_speed) > 3.0:
 		print("big collision")
 		Input.vibrate_handheld(100)
@@ -58,15 +37,41 @@ func _physics_process(delta: float) -> void:
 		for joypad in Input.get_connected_joypads():
 			Input.start_joy_vibration(joypad, 0.5, 0.0, 0.1)
 	
+	# initial accel faster
+	if Input.is_action_pressed("ui_accel"):
+		
+		#print(int(actual_speed)) #DEBUG
+		if actual_speed < initial_min_speed && not is_zero_approx(actual_speed):
+			#print("Max accel") #DEBUG
+			#print(Input.get_action_strength("ui_accel"))
+			engine_force = Input.get_action_strength("ui_accel") * ENGINE_POWER * 2.5
+		else:
+			engine_force = Input.get_action_strength("ui_accel") * ENGINE_POWER
+	
+	elif Input.is_action_pressed("ui_decel"):
+
+		if direction > 0.0:
+			engine_force = -Input.get_action_strength("ui_decel") * ENGINE_POWER * BRAKE_STRENGTH
+		elif actual_speed < initial_min_speed && not is_zero_approx(actual_speed):
+			engine_force = -Input.get_action_strength("ui_decel") * ENGINE_POWER * 2.0
+		else:
+			engine_force = -Input.get_action_strength("ui_decel") * ENGINE_POWER
+	
+	else:
+		engine_force = 0.0
+	
+	if floor_raycast.is_colliding():
+		var surface_normal : Vector3 = floor_raycast.get_collision_normal()
+		apply_force(surface_normal * DOWN_FORCE, Vector3(0, 0, 0))
 
 	#print("direction", direction)
+	steering = move_toward(steering, Input.get_axis("ui_right", "ui_left") * MAX_STEER, delta * 10)
+	
 	previous_speed = actual_speed
 
 func _input(event: InputEvent) -> void:
 	if event.device != player_index:
 		return
 	
-	
-
 #func this_controller(input_action) -> bool:
 	#return input_action is player_index
