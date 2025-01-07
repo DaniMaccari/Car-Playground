146528f8eea77b25561e6d8b10f89c2b0c839a75
DaniPerezUJI <90398305+DaniPerezUJI@users.noreply.github.com>
2025-01-07 22:30:48 +0100

    Update base tree model and script

--- Scripts/tree_script.gd
+++ Scripts/tree_script.gd
@@ -1,5 +1,14 @@
 extends StaticBody3D
 
+@export var tree_models_array : Array[Node3D]
+var tree_selected : int = 0
+
+func _ready() -> void:
+	for model in tree_models_array:
+		model.hide()
+	
+	tree_models_array[tree_selected].show()
+	
 
 func shake_tree() -> void:
 	print("Shake Tree")
