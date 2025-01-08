d51a276489df70edbd6059a37954c08507ea4e0c
DaniPerezUJI <90398305+DaniPerezUJI@users.noreply.github.com>
2025-01-08 22:00:54 +0100

    Create tree randomizer

--- Scripts/tree_script.gd
+++ Scripts/tree_script.gd
@@ -4,10 +4,12 @@ extends StaticBody3D
 var tree_selected : int = 0
 
 func _ready() -> void:
-	for model in tree_models_array:
+	
+	for model in $TreeModel.get_children():
 		model.hide()
 	
-	tree_models_array[tree_selected].show()
+	$TreeModel.get_child(randi_range(0, 4)).show()
+	#tree_models_array[tree_selected].show()
 	
 
 func shake_tree() -> void:
